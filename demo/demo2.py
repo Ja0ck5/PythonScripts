e1966854675efa200c6b7348427092344df3b68f
ja0ck5 <975117619@qq.com>
2017-11-03 14:42:33 +0800

    d2

---
+++ demo/demo2.py
@@ -0,0 +1,124 @@
+# a = 100
+# b=200
+# result = a + b
+# print("result:%d"%result)
+
+
+# password= raw_input("please enter ur password:")
+# print("password:%s"%password)
+
+# c=9
+# d=2.0
+# result = c//d
+# print("result:%d"%result)
+
+# result = c**d
+# print("result:%d"%result)
+
+
+# e='hello'
+# f='world'
+# result = e+f
+# print("result:%s"%result)
+
+# age=20
+# if not 18 < age :
+# 	print("I am a man")
+# print("I am a boy")
+
+# score = 680
+# score = 500
+# if score >= 600 :
+# 	print("University")
+# else :
+# 	print("high school")
+
+
+# if score >= 600 :
+# 	print("University")
+# elif 500 == score :
+# 	print("haha 500")
+# else :		
+# 	print("boo 400")
+
+# target = 1
+# station = 1
+# if 1 == target:
+# 		print("u can pass")	
+
+# 		if 1 == station :
+# 			print("u can go off the car")
+
+# i = 0
+# j=0
+# while i < 100:
+# 	print("Haha%d"%i)
+# 	i+=1
+# 	while j < 100:
+# 		print("JAJA%d"%j)
+# 		if j == 52 : 
+# 			print("now break")
+# 			break
+# 		j+=1
+							
+
+# for i in range(10):
+# 	print("-"*20)
+
+# 	if i == 4 :
+# 		print("now take a break")
+# 		break
+
+# 	print("i=%d"%i)	
+
+
+
+# def printFive():
+# 	print("=========================")
+# 	print("=========================")
+# 	print("=========================")
+# 	print("=========================")
+# 	print("=========================")
+
+# def printName():
+# 	print("=========================")
+# 	print("=========================")
+# 	print("ArmStrong")
+# 	print("=========================")
+# 	print("=========================")	
+
+
+# # call function
+# printFive()
+# printName()
+
+
+str = "hello world"
+mystr = "hello world this is a python script"
+# i=0
+# while i< 11 :
+# 	print("%s"%str[i])
+# 	print("%s"%str[i:11])
+# 	i+=1
+
+# print("%s"%mystr.find("is"))
+
+# print("%d"%mystr.find("is",10,15))
+
+# print("%d"%mystr.count("is"))
+
+# print("%d"%len(mystr))
+
+# print("%s"%mystr.replace("is","si",mystr.count("is")))
+
+arr = ['xioaxiao','dada','zz']
+
+tuple1 = ['xioaxiao','dada','zz',3.14159265358979626,100]
+
+
+
+
+
+
+	
+
