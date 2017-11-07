714be827b4640864f53bf650662932243659814f
ja0ck5 <975117619@qq.com>
2017-11-07 20:16:03 +0800

    file

--- demo/people.py
+++ demo/people.py
@@ -4,6 +4,13 @@ class People:
 	__name = "Tom"
 
 	__age = 12
+	# 构造方法
+	def __init__(self):
+		print('this is a constructor...')
+
+	def __del__(self):
+		print 'now done...'	
+
 
 	def getName(self):
 		return self.__name
@@ -13,7 +20,15 @@ class People:
 
 	def getAge(self):
 		return self.__age
+	
+	def printName(self):
+		print 'name:',self.__name
+
+def myPrint(p):
+	p.printName()		
 
 p = People()
+#del p
+myPrint(p)
+print p.getName,p.getAge
 
-print p.getName,p.getAge			
